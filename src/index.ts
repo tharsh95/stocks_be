d1ac8047415ac312a9efdf715b1ea9d3d84bd70b
Harsh Tiwari <harsh@kimo.ai>
2025-03-02 16:35:39 +0530

    Updated changes

--- src/index.ts
+++ src/index.ts
@@ -4,18 +4,19 @@ import { Server as SocketIOServer } from "socket.io";
 import dotenv from "dotenv"
 import connectToDB from "./config/db";
 import { initSocket } from "./config/socket"
+import cors from "cors";
 
 dotenv.config();
 
 const PORT = process.env.NODE_PORT ?? 8080;
-
 const app: Express = express();
+app.use(cors());
 
 const server: HTTPServer = createServer(app);
 
 export const io: SocketIOServer = new SocketIOServer(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
@@ -26,5 +27,4 @@ server.listen(PORT, () => {
   connectToDB();
   console.log(`Server running on port ${PORT}`);
 });
-
 
